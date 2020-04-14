0885dc386d46c10a7d752040db61557d6b77bc22
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-04-14 20:45:16 -0300

    End of service

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import './global.css'
 
 function App() {
 
+  const [ decimal, setDecimal ] = useState('')
+  const [ binary, setBinary ] = useState('')
+
   function handleConvert(e) {
     e.preventDefault()
+
+    setDecimal(parseInt(binary, 2))
+
     console.log('enviou')
   }
 
   return (
     <main>
-      <textarea></textarea>
+      
       <form onSubmit={handleConvert}>
-        <input required/>
+        <label htmlFor="txtBin">Informe um valor em binario (0 e 1)</label><br/>
+        <input id="txtBin" value={binary} minLength="8" maxLength="8" onChange={e => setBinary(e.target.value)} required/>
         <button type="submit">Converter</button>
       </form>
+      <textarea className="resultView" value={decimal} readOnly></textarea>
     </main>
   );
 }
